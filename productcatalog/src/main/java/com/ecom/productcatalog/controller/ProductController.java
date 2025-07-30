e6a18d492a8074f1cfdd8d69dc1048e698c65cd9
Randimal441 <randimalchamika@gmail.com>
2025-07-30 23:27:16 +0530

    up2

--- productcatalog/src/main/java/com/ecom/productcatalog/controller/ProductController.java
+++ productcatalog/src/main/java/com/ecom/productcatalog/controller/ProductController.java
@@ -1,11 +1,26 @@
 package com.ecom.productcatalog.controller;
 
+import com.ecom.productcatalog.model.Product;
+import com.ecom.productcatalog.service.ProductService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
 
+    private final ProductService productService;
 
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    
 }
