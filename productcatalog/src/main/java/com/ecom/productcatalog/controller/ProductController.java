0588e7cb653cb29a9fbd9bb8856e7e0eb436b23a
Randimal441 <randimalchamika@gmail.com>
2025-07-30 23:13:55 +0530

    Update ProductController.java

--- productcatalog/src/main/java/com/ecom/productcatalog/controller/ProductController.java
+++ productcatalog/src/main/java/com/ecom/productcatalog/controller/ProductController.java
@@ -1,4 +1,11 @@
 package com.ecom.productcatalog.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+
+
 }
