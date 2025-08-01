0a3f390eaab0fce5601f077cfa98b853252601f9
Randimal441 <randimalchamika@gmail.com>
2025-08-02 01:22:23 +0530

    Update CategoryController.java

--- productcatalog/src/main/java/com/ecom/productcatalog/controller/CategoryController.java
+++ productcatalog/src/main/java/com/ecom/productcatalog/controller/CategoryController.java
@@ -5,11 +5,13 @@ import com.ecom.productcatalog.service.CategoryService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CategoryController {
 
     private final CategoryService categoryService;
