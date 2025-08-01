60cdfc8e6726fa4aba352881ab51662f6211f7ee
Randimal441 <randimalchamika@gmail.com>
2025-08-01 23:59:06 +0530

    Update ProductController.java

--- productcatalog/src/main/java/com/ecom/productcatalog/controller/ProductController.java
+++ productcatalog/src/main/java/com/ecom/productcatalog/controller/ProductController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
 
     private final ProductService productService;
