99801fade9a674615dfcdb0693411fdf3f9335e2
Randimal441 <randimalchamika@gmail.com>
2025-08-03 22:00:22 +0530

    Update DataSeeder.java

--- productcatalog/src/main/java/com/ecom/productcatalog/config/DataSeeder.java
+++ productcatalog/src/main/java/com/ecom/productcatalog/config/DataSeeder.java
@@ -40,22 +40,22 @@ public class DataSeeder implements CommandLineRunner {
 
         //creat category
         Product phone = new Product();
-        phone.setName("Smart Phone");
-        phone.setDescription("Letest model");
+        phone.setName("Pixel 8 Pro");
+        phone.setDescription("Google Pixel 8 Pro Android smartphone. Announced Oct 2023. Features 6.7″ display, Google Tensor G3 chipset, 5050 mAh battery, 1024 GB storage, 12 GB RAM");
         phone.setImageUrl("https://fs.npstatic.com/devices/77/3477-w320h320.png");
         phone.setPrice(699.99);
         phone.setCategory(electronics);
 
         Product laptop = new Product();
-        laptop.setName("Laptop");
-        laptop.setDescription("High-performance Laptop");
+        laptop.setName("HP Laptop");
+        laptop.setDescription("HP 15 - Intel® Core™ 5 – 8GB/ 512 SSD · LKR 229,990 ; HP 15 - Intel® Core™ 3 – 8GB/ 512 SSD · LKR 175,990 ; HP Pavilion x360 Laptop – 14th Gen Core 7-150U/ 16GB RAM");
         laptop.setImageUrl("https://media.istockphoto.com/id/479520746/photo/laptop-with-blank-screen-on-white.jpg?s=612x612&w=0&k=20&c=V5dj0ayS8He0BP4x15WR5t5NKYzWTKv7VdWvD2SAVAM=");
         laptop.setPrice(999.99);
         laptop.setCategory(electronics);
 
         Product jacket = new Product();
         jacket.setName("Winter Jacket");
-        jacket.setDescription("Warm jacket");
+        jacket.setDescription("Our exclusive Winter Jacket Collection offers durable, comfortable coats to keep you cozy all season long. Find your perfect parka, down jacket or wool coat");
         jacket.setImageUrl("https://media.istockphoto.com/id/182188051/photo/a-light-blue-winter-jacket-with-hood.jpg?s=612x612&w=0&k=20&c=ywUbJ6kRzppHfXgytC0_mBjWJmchtfKCNeKrSAOCJ10=");
         jacket.setPrice(299.99);
         jacket.setCategory(clothing);
